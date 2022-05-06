f33b8bf0a19ce8f3bcde306369815d85f8a80c3b
rongwenlu <rongwenlu@dns.com>
2022-05-06 11:55:53 +0800

    fix brunch_A 3.0

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("This is master ver_1.0")
 	fmt.Println("This is brunch_A ver 1.0")
 	fmt.Println("this is brunch_A ver 2.0")
-	fmt.Println("this is  brunch_A ver 3.0")
+	fmt.Println("this is brunch_A ver 3.0")
 	fmt.Println("this is master ver_2.0")
 	fmt.Println("this is master ver_3.0")
 	fmt.Println("this is master ver_4.0")
